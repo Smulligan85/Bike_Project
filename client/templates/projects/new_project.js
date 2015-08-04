3bdf74637f5d738a5bcb2858b84e94ee507cdf9a
Sean Mulligan <sean.mulligan85@gmail.com>
2015-08-04 07:43:19 -0700

    Add errors

--- client/templates/projects/new_project.js
+++ client/templates/projects/new_project.js
@@ -6,11 +6,27 @@ Template.newProject.events({
       title: $(e.target).find('[name=title]').val(),
     };
 
+    var errors = validateProject(project);
+    if (errors.title || errors.url)
+      return Session.set('newProjectErrors', errors);
+
     Meteor.call('projectInsert', project, function(error, result) {
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
 
     Router.go('projectPage', {_id: result._id});
     });
+  }
+});
+
+Template.newProject.onCreated(function() {
+  Session.set('newProjectErrors', {});
+});
+Template.newProject.helpers({
+  errorMessage: function(field) {
+    return Session.get('newProjectErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('newProjectErrors')[field] ? 'has-error' : '';
   }
 });
